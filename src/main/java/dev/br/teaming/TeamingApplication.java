94e54abec1c90b8190164c7fa06c31972a2f3c5b
mynameisdaun <daun9870.jung@gmail.com>
2022-05-27 16:43:40 +0900

    init

---
+++ src/main/java/dev/br/teaming/TeamingApplication.java
@@ -0,0 +1,13 @@
+package dev.br.teaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamingApplication.class, args);
+    }
+
+}
