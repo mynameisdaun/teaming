ab67a2fd56778e9c6ffcfb92b27d85574be830f1
mynameisdaun <daun9870.jung@gmail.com>
2022-05-28 14:36:48 +0900

    <feat>(Description.java): create Description VO

---
+++ src/main/java/dev/br/teaming/domain/video/domain/vo/Description.java
@@ -0,0 +1,28 @@
+package dev.br.teaming.domain.video.domain.vo;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
+public class Description {
+
+    private String description;
+
+    public Description(String description) {
+        if(!Strings.isNotBlank(description)) {
+            throw new IllegalArgumentException();
+        }
+        this.description = description;
+    }
+
+    public String getValue() {
+        return description;
+    }
+
+}
