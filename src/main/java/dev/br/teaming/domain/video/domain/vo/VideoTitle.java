9f05bc9a9ae55d475320a4b221f7365da4504a1b
mynameisdaun <daun9870.jung@gmail.com>
2022-05-28 20:32:02 +0900

    <feat>(VideoTitle.java): create VideoTitle VO

---
+++ src/main/java/dev/br/teaming/domain/video/domain/vo/VideoTitle.java
@@ -0,0 +1,27 @@
+package dev.br.teaming.domain.video.domain.vo;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
+public class VideoTitle {
+
+    private String Title;
+
+    public VideoTitle(String Title) {
+        if (!Strings.isNotBlank(Title)) {
+            throw new IllegalArgumentException();
+        }
+        this.Title = Title;
+    }
+
+    public String getValue() {
+        return Title;
+    }
+}
