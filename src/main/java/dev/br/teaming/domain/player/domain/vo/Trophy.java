757d0bba5ef0d98b3138405527f9781ea7f06f1f
mynameisdaun <daun9870.jung@gmail.com>
2022-05-29 10:59:59 +0900

    <feat>(Trophy.java) create Trophy VO

---
+++ src/main/java/dev/br/teaming/domain/player/domain/vo/Trophy.java
@@ -0,0 +1,31 @@
+package dev.br.teaming.domain.player.domain.vo;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
+public class Trophy {
+
+    @Column(name="trophy_current")
+    private int currentTrophy;
+
+    @Column(name="trophy_highest")
+    private int highestTrophy;
+
+    public Trophy(int currentTrophy, int highestTrophy) {
+        if(Objects.isNull(currentTrophy) || Objects.isNull(highestTrophy) || currentTrophy < 0  || highestTrophy < 0 || currentTrophy > highestTrophy) {
+            throw new IllegalArgumentException();
+        }
+        this.currentTrophy = currentTrophy;
+        this.highestTrophy = highestTrophy;
+    }
+}
