8544612aaeeabfdea98b1d831baa237e0039e7fb
mynameisdaun <daun9870.jung@gmail.com>
2022-05-29 16:22:41 +0900

    <feat>(Brawler.java) add create Validation for Brawler
    null check,
    and Integer values must not be negative

--- src/main/java/dev/br/teaming/domain/player/domain/vo/brawler/Brawler.java
+++ src/main/java/dev/br/teaming/domain/player/domain/vo/brawler/Brawler.java
@@ -3,34 +3,54 @@ package dev.br.teaming.domain.player.domain.vo.brawler;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.Embedded;
-import java.util.List;
+import java.util.Objects;
 
 @Embeddable
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Brawler {
+
     @Column(name = "brawler_id")
-    private int brawlerId;
+    private Integer brawlerId;
     @Column(name = "brawler_name")
-    private int brawlerName;
+    private String brawlerName;
     @Column(name = "brawler_power")
-    private int brawlerPower;
+    private Integer brawlerPower;
     @Column(name = "brawler_rank")
-    private int brawlerRank;
+    private Integer brawlerRank;
     @Column(name = "brawler_trophy")
-    private int brawlerTrophy;
+    private Integer brawlerTrophy;
     @Column(name = "brawler_highest_trophy")
-    private int brawlerHighestTrophy;
+    private Integer brawlerHighestTrophy;
     @Embedded
     private Gears gears;
     @Embedded
     private StarPowers starPowers;
     @Embedded
     private Gadgets gadgets;
+
+    public Brawler(Integer brawlerId, String brawlerName, Integer brawlerPower, Integer brawlerRank, Integer brawlerTrophy, Integer brawlerHighestTrophy,
+                   Gears gears, StarPowers starPowers, Gadgets gadgets) {
+        if( Objects.isNull(brawlerId) || brawlerId < 0 || Strings.isBlank(brawlerName) || Objects.isNull(brawlerPower) || brawlerPower < 0 ||
+            Objects.isNull(brawlerRank) || brawlerRank < 0 || Objects.isNull(brawlerTrophy) || brawlerTrophy < 0||Objects.isNull(brawlerHighestTrophy) || brawlerHighestTrophy < 0 ||
+            Objects.isNull(gears) || Objects.isNull(starPowers) || Objects.isNull(gadgets)) {
+            throw new IllegalArgumentException();
+        }
+        this.brawlerId = brawlerId;
+        this.brawlerName = brawlerName;
+        this.brawlerPower = brawlerPower;
+        this.brawlerRank = brawlerRank;
+        this.brawlerTrophy = brawlerTrophy;
+        this.brawlerHighestTrophy = brawlerHighestTrophy;
+        this.gears = gears;
+        this.starPowers = starPowers;
+        this.gadgets = gadgets;
+    }
 
     @Override
     public boolean equals(Object o) {
