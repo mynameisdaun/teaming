3f09ccab680f1e72ac51527b9d467828b0fbfc3c
mynameisdaun <daun9870.jung@gmail.com>
2022-05-29 14:59:37 +0900

    <test>(GadgetsTest.java) test code For Gadgets

--- src/test/java/dev/br/teaming/domain/player/domain/vo/brawler/GadgetsTest.java
+++ src/test/java/dev/br/teaming/domain/player/domain/vo/brawler/GadgetsTest.java
@@ -1,32 +1,19 @@
 package dev.br.teaming.domain.player.domain.vo.brawler;
 
-import dev.br.teaming.domain.video.domain.vo.Thumbnail;
-import dev.br.teaming.domain.video.domain.vo.Thumbnails;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullSource;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 import static dev.br.teaming.domain.fixture.Fixture.gadget;
-import static dev.br.teaming.domain.fixture.Fixture.thumbnail;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class GadgetsTest {
-
-    static Stream<Arguments> wrong_gadget_list() {
-        return Stream.of(
-                Arguments.of(new ArrayList<>(Arrays.asList()))
-        );
-    }
 
     @DisplayName(value = "Gadgets를 생성한다")
     @Test
