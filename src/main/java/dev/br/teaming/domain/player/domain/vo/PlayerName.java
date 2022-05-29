b8d30231d36a2534cb7bdd746b315d98aa0f02c2
mynameisdaun <daun9870.jung@gmail.com>
2022-05-29 10:31:58 +0900

    <test>(PlayerNameTest.java) add color for nickname

--- src/main/java/dev/br/teaming/domain/player/domain/vo/PlayerName.java
+++ src/main/java/dev/br/teaming/domain/player/domain/vo/PlayerName.java
@@ -2,26 +2,31 @@ package dev.br.teaming.domain.player.domain.vo;
 
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.apache.logging.log4j.util.Strings;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 @EqualsAndHashCode
 public class PlayerName {
 
+    @Column(name = "player_name")
     private String name;
+    @Column(name = "player_name_color")
+    private String color;
 
-    public PlayerName(String name) {
-        if (!Strings.isNotBlank(name)) {
+    public PlayerName(String name, String color) {
+        if (!Strings.isNotBlank(name)||!Strings.isNotBlank(color)||color.length()!=10) {
             throw new IllegalArgumentException();
         }
         this.name = name;
+        this.color = color;
     }
 
-    public String getValue() {
-        return name;
-    }
+
 }
