2646cbb5638585a044c40b51f4564296d5b9e8a5
mynameisdaun <daun9870.jung@gmail.com>
2022-05-30 14:33:36 +0900

    <feat> create VideoService

---
+++ src/main/java/dev/br/teaming/domain/video/application/VideoService.java
@@ -0,0 +1,28 @@
+package dev.br.teaming.domain.video.application;
+
+import dev.br.teaming.domain.player.domain.Player;
+import dev.br.teaming.domain.player.domain.vo.PlayerTag;
+import dev.br.teaming.domain.player.repository.PlayerRepository;
+import dev.br.teaming.domain.video.dto.VideoDTO;
+import dev.br.teaming.domain.video.repository.VideoRepository;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@Slf4j
+@Transactional
+@Service
+public class VideoService {
+    private final VideoRepository videoRepository;
+    private final PlayerRepository playerRepository;
+
+    public List<VideoDTO> findAllByPlayerTag(PlayerTag playerTag) {
+        //Player player = playerRepository.findByPlayerTag(playerTag);
+        return null;
+    }
+}
