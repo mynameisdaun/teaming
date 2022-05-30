814d384747f682a633f55f99f42360174c419c4e
mynameisdaun <daun9870.jung@gmail.com>
2022-05-30 15:02:48 +0900

    <feat>(VideoRepository) add findByYoutuberIn feature

--- src/main/java/dev/br/teaming/domain/video/repository/VideoRepository.java
+++ src/main/java/dev/br/teaming/domain/video/repository/VideoRepository.java
@@ -2,13 +2,17 @@ package dev.br.teaming.domain.video.repository;
 
 import dev.br.teaming.domain.video.domain.Video;
 import dev.br.teaming.domain.video.domain.vo.VideoId;
+import dev.br.teaming.domain.video.domain.vo.Youtuber;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface VideoRepository extends JpaRepository<Video, Long> {
 
     Optional<Video> findByVideoId(VideoId videoId);
+
+    List<Video> findByYoutuberIn(List<Youtuber> youtubers);
 }
