fbd5d86d76b96d631eeeb2de3d985c849d1129e2
mynameisdaun <daun9870.jung@gmail.com>
2022-05-30 15:01:56 +0900

    <test>(Fixture.java) add one more video fixture w/ youtuber parameter

--- src/test/java/dev/br/teaming/domain/fixture/Fixture.java
+++ src/test/java/dev/br/teaming/domain/fixture/Fixture.java
@@ -33,6 +33,15 @@ public class Fixture {
         return new Video(videoId, title, description, publishedAt, youtuber, thumbnails);
     }
 
+    public static Video video(Youtuber youtuber) {
+        VideoId videoId = new VideoId("jYMrymKeJY0");
+        VideoTitle title = new VideoTitle("즐거운 테스트 영상 제목");
+        Description description = new Description("안녕하세요 돈안쓰고 게임을 즐길 수 있는 꿀팁들을 전해드리는 브롤입니다! 여러분의 구독 & 좋아요는 영상을 만드는데 정말 큰 힘이 ...");
+        Date publishedAt = new Date();
+        Thumbnails thumbnails = new Thumbnails(Arrays.asList(Fixture.thumbnail()));
+        return new Video(videoId, title, description, publishedAt, youtuber, thumbnails);
+    }
+
     public static Thumbnail thumbnail() {
         return new Thumbnail(ThumbnailType.DEFAULT, "https://i.ytimg.com/vi/ZuPMKJ0G1R0/default.jpg", 120.0, 90.0);
     }
