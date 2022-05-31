0a1b341776c07c90e397f5a2af199cf8887c1496
mynameisdaun <daun9870.jung@gmail.com>
2022-05-31 15:07:57 +0900

    <feat>(VideoRestController, VideoService ) 유튜브에서 정보 조회해오는 api 생성

--- src/main/java/dev/br/teaming/domain/video/api/VideoRestController.java
+++ src/main/java/dev/br/teaming/domain/video/api/VideoRestController.java
@@ -4,11 +4,13 @@ import dev.br.teaming.domain.player.domain.vo.PlayerTag;
 import dev.br.teaming.domain.video.application.VideoService;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RequestMapping("/api/video")
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
@@ -17,10 +19,14 @@ public class VideoRestController {
     private final VideoService videoService;
 
     @GetMapping
-    public ResponseEntity<?> findAll(final HttpRequest request) {
+    public ResponseEntity<?> findAll(final HttpServletRequest request) {
         PlayerTag playerTag = new PlayerTag("#YLUPG8PJP");
         return ResponseEntity.ok(videoService.findAllByPlayerTag(playerTag));
     }
 
-
+    @PostMapping("/manualUpdate")
+    public ResponseEntity<?> manualUpdate(final HttpServletRequest request) {
+        String keyword = "홀릿";
+        return ResponseEntity.ok(videoService.manualVideoUpdate(keyword));
+    }
 }
--- src/main/java/dev/br/teaming/domain/video/application/VideoService.java
+++ src/main/java/dev/br/teaming/domain/video/application/VideoService.java
@@ -4,6 +4,7 @@ import dev.br.teaming.domain.player.domain.Player;
 import dev.br.teaming.domain.player.domain.vo.PlayerTag;
 import dev.br.teaming.domain.player.repository.PlayerRepository;
 import dev.br.teaming.domain.video.dto.VideoDTO;
+import dev.br.teaming.domain.video.infra.YoutubeVideoClient;
 import dev.br.teaming.domain.video.repository.VideoRepository;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -15,13 +16,14 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
-@AllArgsConstructor(access = AccessLevel.PUBLIC)
 @Slf4j
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Transactional
 @Service
 public class VideoService {
     private final VideoRepository videoRepository;
     private final PlayerRepository playerRepository;
+    private final YoutubeVideoClient youtubeVideoClient;
 
     public List<VideoDTO> findAllByPlayerTag(PlayerTag playerTag) {
         Player player = playerRepository.findByPlayerTag(playerTag)
@@ -30,5 +32,10 @@ public class VideoService {
                 .stream()
                 .map(VideoDTO::new)
                 .collect(Collectors.toList());
+    }
+
+    public List<VideoDTO> manualVideoUpdate(String keyword) {
+        youtubeVideoClient.searchYoutubeVideoByKeyWord(keyword);
+        return null;
     }
 }
