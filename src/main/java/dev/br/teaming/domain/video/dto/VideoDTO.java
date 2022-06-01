f64d5d0add5205929086edc25c778965f86c7c3b
mynameisdaun <daun9870.jung@gmail.com>
2022-06-01 21:00:34 +0900

    <feat>(VideoDTO.java) ) create VideoDTO from YoutubeObject

--- src/main/java/dev/br/teaming/domain/video/dto/VideoDTO.java
+++ src/main/java/dev/br/teaming/domain/video/dto/VideoDTO.java
@@ -1,15 +1,20 @@
 package dev.br.teaming.domain.video.dto;
 
+import com.google.api.services.youtube.model.ResourceId;
+import com.google.api.services.youtube.model.SearchResult;
+import com.google.api.services.youtube.model.SearchResultSnippet;
+import com.google.api.services.youtube.model.Thumbnail;
 import dev.br.teaming.domain.video.domain.Video;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.*;
 
 @Getter
 @EqualsAndHashCode
 @ToString
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Slf4j
 public class VideoDTO {
 
     private String videoId;
@@ -37,5 +42,21 @@ public class VideoDTO {
             thumbnails.add(t);
         });
         this.thumbnails=thumbnails;
+    }
+
+    public static VideoDTO from (final SearchResult singleVideo) {
+        List<Map<String, Object>> thumbnails = new ArrayList<>();
+        SearchResultSnippet snippet = singleVideo.getSnippet();
+        for(String type : snippet.getThumbnails().keySet()) {
+            Map<String, Object> map = new HashMap<>();
+            Thumbnail thumbnail = (Thumbnail) snippet.getThumbnails().get(type);
+            map.put("type", type);
+            map.put("url", thumbnail.getUrl());
+            map.put("width",thumbnail.getWidth());
+            map.put("height",thumbnail.getHeight());
+            thumbnails.add(map);
+        }
+        return new VideoDTO(singleVideo.getId().getVideoId(), snippet.getTitle(), snippet.getDescription(), new Date(snippet.getPublishedAt().getValue()),
+                            snippet.getChannelId(), snippet.getChannelTitle(), thumbnails);
     }
 }
